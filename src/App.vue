34bcc24b1c02afd8b9ea0ce86c960c4f862865c4
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-03-10 18:57:21 -0500

    introduction to vue components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    Hello, Vue!
-  </div>
+    <!-- <ButtonCounter></ButtonCounter> 
+    <ButtonCounter></ButtonCounter>
+    
+    <Button-Counter></Button-Counter>
+    <hr /> -->
+    
+    <div style="background-color: orange; padding: 5px">
+        This is the root component.
+    </div>
+    
+    <ComponentA></ComponentA>
+    <ComponentA></ComponentA>
 </template>
 
 <script setup>
+//buttons above are two instance of a class called buttoncounter and therefor do not interfer
+import ButtonCounter from './ButtonCounter.vue';
+import ComponentA from '@/ComponentA.vue';
+
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped> </style>
