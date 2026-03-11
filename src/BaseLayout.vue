7f655c704ccb760307fd061fcb70540874b5cd1e
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-03-11 18:03:49 -0500

    named slots introduction

---
+++ src/BaseLayout.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+     <header>
+         <!-- we want header content here --> 
+         <slot name="header"></slot>
+     </header>
+     <main>
+         <!-- we want main content here --> 
+         <slot name="main"></slot>
+     </main>
+     <footer>
+         <!-- we want footer content here --> 
+         <slot name="footer"></slot>
+     </footer>
+ </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
