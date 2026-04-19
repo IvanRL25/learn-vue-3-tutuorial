0c415c84473fa64b85258c41da37e4729537203f
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-04-19 18:20:49 -0500

    lazy loading routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// Import the components that we want to route to
-import Home from '@/views/Home.vue'
-import BlogPosts from '@/views/BlogPosts.vue'
-import About from '@/views/About.vue'
-import BlogPost from '@/views/BlogPost.vue'
-import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
-import NotFound from '@/views/NotFound.vue'
-import Ads from '@/views/Ads.vue'
-import Login from '@/views/Login.vue'
-import MainLayout from '@/views/MainLayout.vue'
+// No need to import the components here, they will be lazy-loaded
 import { isAuthenticated } from '@/apis/auth'
 
-// Create a router instance
+
 const router = createRouter({
   // Provide the history implementation to use. We are using the HTML5 history API
   history: createWebHistory(),
-  scrollBehavior(to, from, savePosition) {
-    
+  scrollBehavior(to, from, savedPosition) {
     const scrollBehaviorOptions = {
       top: 0,
       behavior: 'smooth',
     }
-    
+
     if (to.meta.scrollToElement) {
       scrollBehaviorOptions.el = to.meta.scrollToElement
     }
 
-    return savePosition ?? scrollBehaviorOptions
+    return savedPosition ?? scrollBehaviorOptions
   },
-  // Define some routes, each route record should map to a component
+
   routes: [
     {
       path: '/',
       name: 'mainLayout',
-      component: MainLayout,
+      component: () => import('@/views/MainLayout.vue'),
       redirect: { name: 'home' },
       children: [
         {
           path: '/home',
           name: 'home',
-          component: Home,
+          component: () => import('@/views/Home.vue'),
           meta: { requiresAuth: false },
         },
         {
           path: '/blogPosts',
           name: 'blogPosts',
-          component: BlogPosts,
+          component: () => import('@/views/BlogPosts.vue'),
           meta: {
             enterAnimation: 'animate__animated animate__bounceIn',
             leaveAnimation: 'animate__animated animate__bounceOut',
@@ -56,24 +46,27 @@ const router = createRouter({
             {
               path: '',
               name: 'blogPostsGreeting',
-              component: BlogPostsGreeting,
+              component: () => import('@/views/BlogPostsGreeting.vue'),
               meta: { requiresAuth: false },
             },
             {
               path: '/blogPosts/:id(\\d+)',
               name: 'blogPost',
               components: {
-                default: BlogPost,
-                sidebar: Ads,
+                default: () => import('@/views/BlogPost.vue'),
+                sidebar: () => import('@/views/Ads.vue'),
               },
-              meta: { requiresAuth: true, scrollToElement:'.blog-posts-layout'},
+              meta: {
+                requiresAuth: true,
+                scrollToElement: '.blog-posts-layout',
+              },
             },
           ],
         },
         {
           path: '/about',
           name: 'about',
-          component: About,
+          component: () => import('@/views/About.vue'),
           meta: { requiresAuth: false },
         },
       ],
@@ -81,13 +74,13 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: Login,
+      component: () => import('@/views/Login.vue'),
       meta: { requiresAuth: false },
     },
     {
       path: '/:pathMatch(.*)*', // Match any path that hasn't been matched by a previous route
       name: 'notFound',
-      component: NotFound,
+      component: () => import('@/views/NotFound.vue'),
       meta: { requiresAuth: false },
     },
   ],
@@ -101,10 +94,8 @@ router.beforeEach((to, from) => {
   }
 })
 
-// Global after each navigation guard (for cleanup or loggin)
 router.afterEach((to, from) => {
   console.log(`Successfully navigated to: ${to.fullPath}`)
 })
 
-// Export the router instance
 export default router
